8008fc1559f1295de38ebf151bae22b3b60e43d1
chenmax743 <chenmax38@gmail.com>
2023-11-23 14:45:42 +0800

    change text

---
+++ Mazegame/Assets/Script/DisplayTextOnSpecificColorCollision.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayTextOnCollision : MonoBehaviour
+{
+    public string displayText = "Hello, World!"; // 要顯示的文字
+    public Text displayUIText; // 引用 Text 元件
+
+    private void Start()
+    {
+        displayUIText.text = ""; // 清空 Text 元件的文字
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            displayUIText.text = displayText; // 將文字設置到 Text 元件上
+        }
+    }
+}
